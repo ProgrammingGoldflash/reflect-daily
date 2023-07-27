1068fac5486aa8b2a9df6530630edfdf83ec2203
ProgrammingGoldflash <philip.ainberger@aon.at>
2023-07-27 20:14:00 +0200

    integrate first day of week option to calendar

--- src/PA.Reflect.Daily.Web/Pages/Calendar.razor.cs
+++ src/PA.Reflect.Daily.Web/Pages/Calendar.razor.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components;
 using PA.Reflect.Daily.Core.Aggregates;
 using PA.Reflect.Daily.Core.Interfaces;
 using PA.Reflect.Daily.Core.ProjectAggregate;
 using PA.Reflect.Daily.Web.Interfaces;
+using PA.Reflect.Daily.Web.Options;
 using System.Globalization;
 
 namespace PA.Reflect.Daily.Web.Pages;
@@ -12,12 +14,14 @@ public partial class Calendar
   [CascadingParameter] public User User { get; set; } = new();
   [Inject] public IUserService UserService { get; set; } = default!;
   [Inject] public IUnitOfWork UnitOfWork { get; set; } = default!;
+  [Inject] public ILocalStorageService LocalStorage { get; set; } = default!;
 
   private int weeksInMonth => (int)Math.Ceiling((startOfMonth.AddMonths(1) - startOfMonth.AddDays(-(int)startOfMonth.DayOfWeek)).TotalDays / 7);
   private List<Reflection> reflections = new();
   private DateTime startOfMonth { get; set; }
-  
+
   private int[] months = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+  private LocalStorageOptions options = default!;
   private int currentMonth = DateTime.Now.Month;
   private int currentYear = DateTime.Now.Year;
   private DayOfWeek[] days = default!;
@@ -31,6 +35,21 @@ public partial class Calendar
 
     StateHasChanged();
     return;
+  }
+
+  protected override async Task OnAfterRenderAsync(bool firstRender)
+  {
+    if (firstRender && await LocalStorage.ContainsOptionsAsync())
+    {
+      options = await LocalStorage.GetOptionsAsync();
+
+      // move sunday to last index
+      if (options.UseSundayAsFirstDayOfWeek is false)
+      {
+        days = days.Skip(1).Concat(days.Take(1)).ToArray();
+        StateHasChanged();
+      }
+    }
   }
 
   private void PreviousYear()
