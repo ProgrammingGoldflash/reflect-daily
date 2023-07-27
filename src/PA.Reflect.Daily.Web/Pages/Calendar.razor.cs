ddfbfd31d05be058d9c42e0c04f8c6fd96ddcc48
ProgrammingGoldflash <philip.ainberger@aon.at>
2023-07-27 17:47:02 +0200

    outsource code blocks into code behind classes

---
+++ src/PA.Reflect.Daily.Web/Pages/Calendar.razor.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Components;
+using PA.Reflect.Daily.Core.Aggregates;
+using PA.Reflect.Daily.Core.Interfaces;
+using PA.Reflect.Daily.Core.ProjectAggregate;
+using PA.Reflect.Daily.Web.Interfaces;
+using System.Globalization;
+
+namespace PA.Reflect.Daily.Web.Pages;
+
+public partial class Calendar
+{
+  [CascadingParameter] public User User { get; set; } = new();
+  [Inject] public IUserService UserService { get; set; } = default!;
+  [Inject] public IUnitOfWork UnitOfWork { get; set; } = default!;
+
+  private int weeksInMonth => (int)Math.Ceiling((startOfMonth.AddMonths(1) - startOfMonth.AddDays(-(int)startOfMonth.DayOfWeek)).TotalDays / 7);
+  private List<Reflection> reflections = new();
+  private DateTime startOfMonth { get; set; }
+  
+  private int[] months = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+  private int currentMonth = DateTime.Now.Month;
+  private int currentYear = DateTime.Now.Year;
+  private DayOfWeek[] days = default!;
+
+  protected override async Task OnParametersSetAsync()
+  {
+    startOfMonth = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1);
+    days = Enum.GetValues<DayOfWeek>();
+
+    reflections = await UnitOfWork.ReflectionRepository.ListAsync(new ReflectionsByUserId(User.Id));
+
+    StateHasChanged();
+    return;
+  }
+
+  private void PreviousYear()
+  {
+    startOfMonth = startOfMonth.AddYears(-1);
+    currentMonth = startOfMonth.Month;
+    currentYear = startOfMonth.Year;
+  }
+
+  private void NextYear()
+  {
+    startOfMonth = startOfMonth.AddYears(1);
+    currentMonth = startOfMonth.Month;
+    currentYear = startOfMonth.Year;
+  }
+
+  private void SetMonth(int month)
+  {
+    startOfMonth = new DateTime(startOfMonth.Year, month, 1);
+    currentMonth = startOfMonth.Month;
+  }
+
+  private int GetWeekOfYear(DateTime dt)
+  {
+    var calendar = CultureInfo.InvariantCulture.Calendar;
+    return calendar.GetWeekOfYear(dt, CalendarWeekRule.FirstDay, DayOfWeek.Sunday);
+  }
+}
---
+++ src/PA.Reflect.Daily.Web/Pages/ReflectionDetails.razor.cs
@@ -0,0 +1,90 @@
+﻿using Blazorise;
+using Blazorise.Snackbar;
+using Microsoft.AspNetCore.Components;
+using PA.Reflect.Daily.Core.ProjectAggregate;
+using PA.Reflect.Daily.ServiceLayer.Interfaces;
+
+namespace PA.Reflect.Daily.Web.Pages;
+
+public partial class ReflectionDetails
+{
+  [Inject] public IReflectionService ReflectionService { get; set; } = default!;
+  [Inject] public NavigationManager navigationManager { get; set; } = default!;
+  [CascadingParameter] public User User { get; set; } = new();
+  [Parameter] public string Date { get; set; } = string.Empty;
+
+  private Snackbar snackbarPrimary = new();
+  private Reflection reflection = new();
+  private Modal modalRef = new();
+  private bool isNew = true;
+  private Note note = new();
+  private DateOnly dateOnly;
+
+  protected override async Task OnParametersSetAsync()
+  {
+    dateOnly = DateOnly.ParseExact(Date, "yyyy-MM-dd");
+    reflection = new(default, dateOnly) { Id = default };
+
+    var entity = await ReflectionService.GetReflectionAsync(User.Id, dateOnly);
+
+    SetReflection(entity);
+
+    StateHasChanged();
+  }
+
+  protected Task<string> PreviewRender(string plainText)
+  {
+    return Task.FromResult(Markdig.Markdown.ToHtml(note.MarkdownValue ?? string.Empty));
+  }
+
+  private void OpenModal() => modalRef.Show();
+  private Task CancelModal() => modalRef.Hide();
+
+  private void SetReflection(Reflection? entity)
+  {
+    if (entity != null)
+    {
+      reflection = entity;
+      note = reflection.Notes.FirstOrDefault() ?? new();
+      isNew = false;
+    }
+    else
+    {
+      reflection = new Reflection(User.Id, dateOnly);
+      note = reflection.AddNote("Test");
+    }
+  }
+
+  private async Task Delete()
+  {
+    await ReflectionService.DeleteReflectionAsync(reflection);
+
+    await modalRef.Hide();
+
+    navigationManager.NavigateTo("/");
+  }
+
+  private async Task Save()
+  {
+    if (isNew)
+    {
+      var entity = await ReflectionService.AddReflectionAsync(reflection);
+
+      SetReflection(entity);
+      StateHasChanged();
+    }
+    else
+    {
+      await ReflectionService.UpdateReflectionAsync(reflection);
+    }
+
+    await snackbarPrimary.Show();
+  }
+
+  Task OnMarkdownValueChanged(string value)
+  {
+    note.MarkdownValue = value;
+
+    return Task.CompletedTask;
+  }
+}
